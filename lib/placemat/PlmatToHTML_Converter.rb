d8e7a7ea793b257fab15d30d8e85dd774e481555
william-index <thewilliamanderson@gmail.com>
2015-04-13 19:51:52 -0400

    Finishes implementation of #1 Basic Table Parsing

--- lib/placemat/PlmatToHTML_Converter.rb
+++ lib/placemat/PlmatToHTML_Converter.rb
@@ -23,6 +23,7 @@ class PlmatToHTML_Converter
     plmat_string = strip_full_line_comments(plmat_string)
     plmat_string = split_apart_tables(plmat_string)
     plmat_string = split_rows(plmat_string)
+    plmat_string = format_rows(plmat_string)
 
     base_file_name = plmat.slice(0..(plmat.index('.')-1))
     generator.output(plmat_string, base_file_name+".html")
@@ -48,8 +49,6 @@ class PlmatToHTML_Converter
   ##
   # Splits a table intro two sections for header content
   # and body content based on a line of pure hyphens
-  #
-  # Any line beginning with spaces or tabs becomes a part of the previous line
   #
   # Splits Each content section by line break into rows
   #
@@ -107,4 +106,100 @@ class PlmatToHTML_Converter
     row
   end
 
+  ##
+  # Handles call for all formatting of contents within any given row
+  #
+  # Arguments:
+  #   tables (Array) - array containing all table data
+  #
+  # Returns:
+  #   (String) - full formatted html table contents
+  def format_rows(tables) #:doc:
+    tables.each_with_index do |sections, i|
+      sections = format_sections(sections)
+      tables[i] = build_table(sections)
+    end
+    tables
+  end
+
+  ##
+  # Formats each section of a table by wrapping cells
+  # and rows with appropraite tags
+  # This trickles through formatting, for all deeper levels as well
+  #
+  # Arguments:
+  #   sections (Array) - array of sections, where each section is an array of rows
+  #
+  # Returns:
+  #   (Array) - array fo strings, one string per section
+  def format_sections(sections) #:doc:
+    tag_array = ["th","td"]
+    if sections.length == 1
+      tag_array = ["td"]
+    end
+    sections.each_with_index do |section, i|
+      section.each_with_index do |row, j|
+        cells = row.split("|")
+        cells.each_with_index do |cell, k|
+          cells[k] = format_cell(cell, tag_array[i])
+        end
+        sections[i][j] = wrap_row(cells)
+      end
+    end
+    sections
+  end
+
+  ##
+  # Joins cells into single string and wraps in row tag
+  #
+  # Arguments:
+  #   cells (Array)
+  def wrap_row(cells) #:doc:
+    if cells.length > 0
+      "\t<tr> "+cells.join("")+ " </tr>"
+    end
+  end
+
+  ##
+  # Finalizes formatting on the cell level
+  # This also parses out the plmat attributes to be passed
+  # to the attribtue formatter (build_cell_attrs)
+  #
+  # If the X attribute is set however the cell will not be
+  # rendered
+  #
+  # Arguments:
+  #   cell (String) - raw contents of a cell
+  #   tag (String)  - html tag to use for cell
+  def format_cell(cell, tag="td") #:doc:
+    raw_cell = cell.strip
+    cell_parts = raw_cell.split("::")
+    attr_args = cell_parts.fetch(1,"").split("&")
+
+    if attr_args.include? "X"
+      ""
+    else
+      attrs = build_cell_attrs(attr_args)
+      "<#{tag}#{attrs}>#{cell_parts[0]}</#{tag}>"
+    end
+  end
+
+  ##
+  # Builds tag attributes for a cell based on passed arguments
+  #
+  # Arguments:
+  #   args (Array) - all arguments passed from the .plmat argument block
+  def build_cell_attrs(args) #:doc:
+    ""
+  end
+
+  ##
+  # Combines sections and wraps sections in a table tag
+  #
+  # Arguments:
+  #   sections (Array) - @see return format_section
+  #
+  def build_table(sections) #:doc:
+    "<table>\n"+sections.join("\n")+"\n</table>\n\n"
+  end
 end
